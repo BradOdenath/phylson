5a13a63cff2b00ba324fcca6fcf18ff0237bfe56
Brad Odenath <brad.odenath@gmail.com>
2022-02-06 09:42:21 -0500

    sdaf

---
+++ languages/programming_languages/go/go_keyword_list.lua
@@ -0,0 +1,3 @@
+{"break" : "break""case" : "case""chan" : "chan""const" : "const""continue" : "continue""default" : "default""defer" : "defer""else" : "else""fallthrough" : "fallthrough""for" : "for""func" : "func""go" : "go""goto" : "goto""if" : "if""import" : "import""interface" : "interface""map" : "map""package" : "package""range" : "range""return" : "return""select" : "select""struct" : "struct""switch" : "switch""type" : "type""var" : "var"
+}
+
--- languages/programming_languages/go/keyword_table_generator.lua
+++ languages/programming_languages/go/keyword_table_generator.lua
@@ -1,45 +1,63 @@
 
+
+-- Put quotes arounds a string value
+stringify = function(str)     
+	if (str) then
+		if (type(string.lower(str)) == string.lower('string')) then
+			return tostring([["]]..str..[["]])
+		else
+			print('dif_value: essentials/stringify/arg/str: '..tostring(str))
+		end
+	else
+		print('nil_value: essentials/stringify/arg/str')
+	end
+	return str
+end
+
+equalmyself = function(thing)
+	return (thing..[[ : ]]..thing)
+end
+print(equalmyself(stringify('ayoo')))
+
+
 luajson = function(file_name)
 
 	if (file_name) then
 		local file = io.open(file_name, "r")
 		local outStr
+		local outTable = {}
 		
 		if (file) then
 			io.input(file)
 			outStr = "{"
-
-			local status, line
-			while true do 
-				status, line = pcall(function() return io.read() end)\
-				print(status, line)
-				if (status) then
-					if (line and #line > 1) then
-						print("Line "..line)
-						outStr = (
-							outStr
-								..([[\n\t{ "]]
-								..tostring(line)
-								..'" = "'
-								..tostring(line)
-								..[["};]])
-						)
-					else
-						print("err_value: keyword_table_generator/luajson/var/line")
-					end
-				
-					print("err_value: keyword_table_generator/luajson/var/status")
-					break
+			for line in io.lines(file_name) do
+				if (line) then
+					table.insert(outTable, tostring(line))
+				else
+					print("err_value: keyword_table_generator/luajson/var/line: "..tostring(line))
 				end
+					
 			end
 			
 			io.close(file)
+			
+			for i,v in pairs(outTable) do			
+				local _os = v:gsub("\n","")
+				print(_os)
+				_os = stringify(_os)
+				print(_os)
+				local shouldequalitself = equalmyself(_os)
+				print(tostring(shouldequalitself))
+				outStr = (
+					outStr
+						..shouldequalitself
+				)
+			end
 
-			outStr = (outStr.."\n}")
+			outStr = (outStr.."\n}\n\n")
 		else
 			print("nil_value: keyword_table_generator/luajson/var/file")
 		end
-
 		return outStr
 	else
 		print("nil_value: keyword_table_generator/luajson/arg/file_name")
@@ -59,19 +77,21 @@ keyword_list_generator = function(file_name)
 			if (keyword_list_file) then
 				io.input(keyword_list_file)
 				
-				io.output(file)
+				io.output(keyword_list_file)
 				
 				io.write(generated_keywords_table)
 				
 				io.close(keyword_list_file)
-				
-				print("nil_value: keyword_table_generator/keyword_list_generator/var/keyword_list_file")
-			end
-			print("nil_value: keyword_table_generator/keyword_list_generator/var/generated_keywords_table")
 			
+			else
+				print("nil_value: keyword_table_generator/keyword_list_generator/var/keyword_list_file: "..tostring(keyword_list_file))
+			end
+		else
+			print("nil_value: keyword_table_generator/keyword_list_generator/var/generated_keywords_table: "..tostring(generated_keywords_table))
 		end
-		print("nil_value: keyword_table_generator/keyword_list_generator/arg/file_name")
+	else
+		print("nil_value: keyword_table_generator/keyword_list_generator/arg/file_name: "..tostring(file_name))
 	end		
 end
 
-main = function() print('main') keyword_list_generator("go_keywords.txt") end main()
+main = function()  keyword_list_generator("go_keywords.txt") end main()
