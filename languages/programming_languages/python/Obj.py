96663e198327e3e9bfa95f32b7b2613b3cd8d274
Brad Odenath <brad.odenath@gmail.com>
2022-02-23 09:50:07 -0500

    asdf

--- languages/programming_languages/python/Obj.py
+++ languages/programming_languages/python/Obj.py
@@ -1,31 +1,31 @@
 @dataclass
 class Obj:
 
+	# Declare attribute ihntegers as data type"int" and initialize the value as "4, 5, 6".
+	ihntegers : int = [4, 5, 6]
+
+	# Declare attribute ihnt as data type "int" and initialize the value as 5.
+	ihnt : int = 5
+
 	# Declare attribute sthr as data type "string" and initialize the value as sssttrrrrr.
 	sthr : str = sssttrrrrr
 
 	# Declare attribute dubbl as data type "double" and initialize the value as 13.37.
 	dubbl : double = 13.37
 
-	# Declare attribute ihnt as data type "int" and initialize the value as 5.
-	ihnt : int = 5
-
-	# Declare attribute ohbjhct as data type"Ooobbbjjjeeecccttt" and initialize the value as "table: 0x7fffe2f58ff0, table: 0x7fffe2f590c0, table: 0x7fffe2f58f20".
-	ohbjhct : Ooobbbjjjeeecccttt = [table: 0x7fffe2f58ff0, table: 0x7fffe2f590c0, table: 0x7fffe2f58f20]
-
-	# Declare attribute suhpher_ohbjhekt as data type"VEEEHIKL" and initialize the value as "".
-	suhpher_ohbjhekt : VEEEHIKL = []
-
-	# Declare attribute ihntegers as data type"int" and initialize the value as "4, 5, 6".
-	ihntegers : int = [4, 5, 6]
+	# Declare attribute ohbjhct as data type"Ooobbbjjjeeecccttt" and initialize the value as "table: 0x7fffe99abdc0, table: 0x7fffe99abcf0, table: 0x7fffe99abe90".
+	ohbjhct : Ooobbbjjjeeecccttt = [table: 0x7fffe99abdc0, table: 0x7fffe99abcf0, table: 0x7fffe99abe90]
 
 	# Declare attribute chyr as data type "char" and initialize the value as a.
 	chyr : str = a
 
+	# Declare attribute suhpher_ohbjhekt as data type"VEEEHIKL" and initialize the value as "".
+	suhpher_ohbjhekt : VEEEHIKL = []
+
 """
 
-# Declare function as fuhnktchyon with parameters: derpaderp, asdf and return data type "int".
-def fuhnktchyon (derpaderp, asdf):
+# Declare function as fuhnktchyon with parameters: asdf, derpaderp and return data type "int".
+def fuhnktchyon (asdf, derpaderp):
 				if (adf) {
 					derpaderp += adf;
 				} 
