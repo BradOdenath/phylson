ddef41dbe315afba1c67ca73bde5fe64774aab18
Brad Odenath <brad.odenath@gmail.com>
2022-02-28 21:26:56 -0500

    h

--- essentialz/referencez/reserved_wordz/reserved_wordz.lua
+++ essentialz/referencez/reserved_wordz/reserved_wordz.lua
@@ -20,6 +20,14 @@ zpcall = function(...)
 	return {status = _status, response = _response}
 end
 
+obfuscate = function(code) --glot.io
+	local outLS = (code:gsub(".", function(_) return ("\\".._:byte()) end) or code)
+	--print(outLS)
+	--print('loadstring("'..tostring(outLS)..'")()')
+	return outLS
+end
+obfuscate([[print('hi')]])
+
 literally_the_alphabet = {
 	"a";
 	"b";
@@ -49,17 +57,17 @@ literally_the_alphabet = {
 	"z";
 }
 
-literally_the_alphabet.is_letter = function(letter) 
+literally_the_alphabet.is_letter_value = function(letter) 
 	if (letter) then
 		for i,v in pairs(literally_the_alphabet) do
 			if (string.lower(letter) == v) then
 				return i
 			end
 		end
-		return false
 	else
-		print("nil_value: reserved_wordz/is_letter/arg/letter")
+		print("nil_value: reserved_wordz/is_letter_value/arg/letter")
 	end
+	return 0
 end
 
 
@@ -71,7 +79,7 @@ string_to_table = function(stryng)
 			for i = 1, (#stryng) do
 				table.insert(outTable, stryng[i])
 			end
-			print(outTable[i])
+			--print(outTable[i])
 		else
 			print("typ_value: reserved_wordz/string_to_table/arg/stryng: type"..tostring(type(stryng)))		
 		end
@@ -83,52 +91,211 @@ string_to_table = function(stryng)
 	end
 end
 
+string_to_table_values = function(stryng)
+	if (stryng) then
+		local outTable = {}
+		
+	--[[if (type(stryng) == "string") then]]
+	-- Note: indent ifffffffff statement and comment line below
+		stryng = tostring(stryng)
+			
+		for i = 1, (#stryng) do
+			local lytr = (stryng[i])
+			table.insert(outTable, (literally_the_alphabet.is_letter_value(lytr)))
+		end
+		--print(outTable[i])
+
+	--[[else
+			print("typ_value: reserved_wordz/string_to_table_values/arg/stryng: type"..tostring(type(stryng)))		
+		end]]
+		
+		return outTable
+	else	
+		print('nil_values: reserved_wordz/string_to_table_values/arg/stryng')
+	end
+end
+
 keywords_length = function(taybl)
-	local length
-	for i,v in ipairs(taybl) do
-		if (type(v) == "string") then
-			if (length) then
-				if (length < #v) then
+	if (taybl) then
+		local length
+		for i,v in ipairs(taybl) do
+			if (type(v) == "string") then
+				if (length) then
+					if (length < #v) then
+						length = #v
+					end
+				else
 					length = #v
 				end
-			else
-				length = #v
 			end
 		end
+		return length
+	else
+		print('nil_value: reserved_wordz/keywords_length/arg/taybl')
 	end
-	return length
 end
 
 table_value_alphabeticallify = function(taybl)
 	if (taybl) then
-		local outTaybl = {}
-
-
-		for _, letter in pairs(literally_the_alphabet) do
-			local letter_words = {}
-			
-			local _sorter = function(ouhtTaybl)
-				for i = 1, (keywords_length(ouhtTaybl))
-					for i,v in pairs(ouhtTaybl) do
-						for j,w in pairs(v) do
-							if (type(w) == "string") then
-								
-							end
-						end			
-					end
-				end
-			end
-			local sorter = function(...) _sorter(...) end
-			letter_words = sorter(outTaybl)
-		
+		--print(#taybl)
+	
+		for i,v in pairs(taybl) do
+			table.insert(outTable, string_to_table_values(v) )
 		end
 		
-		return (outTaybl)
+		table.sort(taybl)
+		
+		return (taybl)
 	else
 		print("nil_value: reserved_wordz/table_value_alphabeticallify/arg/taybl")
 	end
 end
 
+split_string = function(str, delimeter)
+	if (delimeter == nil) then 
+		delimeter = '%s'
+	end
+	local result = {}
+	for match in (str..delimeter):gmatch('(.-)'..delimeter) do
+		table.insert(result, match)
+	end
+	return result
+end
+
+-- Put quotes arounds a string value
+stringify = function(str)     
+	if (str) then
+		--str = split_string(str, ' ')[1]
+		str = (string.sub(str,1,#str-1))
+		if (type(string.lower(str)) == string.lower('string')) then
+			return tostring([["]]..str..[["]])
+		else
+			print('dif_value: essentials/stringify/arg/str: '..tostring(str))
+		end
+	else
+		print('nil_value: essentials/stringify/arg/str')
+	end
+	return str
+end
+
+equalmyself = function(thing)
+	--thing = stringify(thing)
+	return (
+		"\t"
+		..stringify(obfuscate(thing))..[[ : ]]..stringify(thing)
+		..";"
+		.."\n"
+	)
+end
+print(equalmyself('ayoo'))
+
+luajson = function(file_name)
+
+	if (file_name) then
+		local file = io.open(file_name, "r")
+		local outStr
+		local outTable = {}
+		
+		if (file) then
+			io.input(file)
+			outStr = "{\n"
+			for line in io.lines(file_name) do
+				if (line) then
+					table.insert(outTable, tostring(line))
+				else
+					print("err_value: keyword_table_generator/luajson/var/line: "..tostring(line))
+				end
+					
+			end
+			
+			io.close(file)
+			
+			for i,v in pairs(outTable) do			
+				local _os = v:gsub("\n","")
+				local shouldequalitself = equalmyself(_os)
+				outStr = (
+					outStr
+						--.."\t"..stringify(_os)..";\n"
+						..shouldequalitself
+				)
+			end
+
+			outStr = (outStr.."}\n\n")
+		else
+			print("nil_value: keyword_table_generator/luajson/var/file")
+		end
+		return outStr
+	else
+		print("nil_value: keyword_table_generator/luajson/arg/file_name")
+	end
+
+
+end
+
+keyword_list_generator = function(file_name)
+	if (file_name) then
+	
+		local generated_keywords_table = (file_name)
+		
+		if (generated_keywords_table) then
+		
+			local keyword_list_file = io.open(string.sub(file_name,1,#file_name-4).."_list.json","w+")
+			
+			if (keyword_list_file) then
+				io.input(keyword_list_file)
+				
+				io.output(keyword_list_file)
+				
+				io.write(generated_keywords_table)
+				
+				io.close(keyword_list_file)
+
+			else
+				print("nil_value: keyword_table_generator/keyword_list_generator/var/keyword_list_file: "..tostring(keyword_list_file))
+			end
+		else
+			print("nil_value: keyword_table_generator/keyword_list_generator/var/generated_keywords_table: "..tostring(generated_keywords_table))
+		end
+	else
+		print("nil_value: keyword_table_generator/keyword_list_generator/arg/file_name: "..tostring(file_name))
+	end		
+end
+
+--Returns position/s in the list
+table.find = function(taybl, item)
+	local positions = {}
+	
+	for i,v in pairs(taybl) do
+		if (v == item) then
+			table.insert(positions, i)
+		end
+	end
+	
+	if (positions <= 0) then
+		positions = nil
+	end
+	return (positions)
+end
+
+table.unionify = function(taybl)
+	
+end
+
+--[[
+remove_duplicates = function(this_poor_table_man)
+	if (this_poor_table_man) then
+		local outTable = {}
+		
+		for i,v in pairs(this_poor_table_man) do
+			
+		end
+		
+		return (outTable)
+	else
+		print('nil_value: reserved_wordz/remove_duplicates/arg/this_poor_table_man')
+	end
+end
+]]
 
 file_list_to_union = function(file_name)
 	if (file_name) then
@@ -145,6 +312,7 @@ file_list_to_union = function(file_name)
 					
 					if (not (outTable[line])) then
 						outTable[line] = tostring(line)
+						print('Added: '..outTable[line])
 					end	
 					--print('asdf'..outTable[line])
 				
@@ -158,6 +326,8 @@ file_list_to_union = function(file_name)
 			--outTable = table_value_alphabeticallify(outTable)
 			table.sort(outTable)
 			
+			print(#outTable)
+			
 			for i,v in pairs(outTable) do
 				local _os = v:gsub("\n","")
 				outStr = (
@@ -168,7 +338,7 @@ file_list_to_union = function(file_name)
 			end
 			
 			--print(outStr)
-			os.execute("cat << "..outStr.." > keywordz.txt")
+			os.execute("cat << "..outStr.." > "..file_name)
 			
 			return outTable
 		end
@@ -177,4 +347,8 @@ file_list_to_union = function(file_name)
 	end
 end
 
-main = function() file_list_to_union("keywordz.txt") end main() 
+main = function() 
+	local fyle_naym = "keywordz.txt"
+	file_list_to_union(fyle_naym) 
+	keyword_list_generator(fyle_naym)
+end main() 
--- essentialz/referencez/reserved_wordz/reserved_wordz.sh
+++ essentialz/referencez/reserved_wordz/reserved_wordz.sh
@@ -1,6 +1,19 @@
 #get all files that are reserved word files ending in _keywords and *.txt
 #each file can be found in languages/programming_languages/[language]
 
+
+check_and_create() {
+	if [ -f "$1" ]
+	then
+		rm "$1"
+		touch "$1"
+		ls
+	fi
+}
+
+check_and_create keywordz.txt
+check_and_create keywordz_list.json
+
 cd ..
 cd ..
 cd ..
@@ -22,4 +35,4 @@ done
 cd ../../essentialz/referencez/reserved_wordz
 ls
 lua reserved_wordz.lua
-#cat keywordz.txt
+#cat keywordz_list.json
