fdd982c365ec0c7190df043bedfc0d2e74f78229
Brad Odenath <brad.odenath@gmail.com>
2022-03-07 00:21:11 -0500

    l

--- languages/programming_languages/perl/perl.lua
+++ languages/programming_languages/perl/perl.lua
@@ -1,3 +1,98 @@
-require "essentialz/essentialz/"
-			
-			
+require "essentialz/essentialz"
+
+local perl_components = {
+	extension_statement = ".pl";
+	comment_statement = "#";
+	finish_statement = ";";
+	function_class = "sub";
+	
+}
+
+perl_class = function(class_data)
+	local outAtr, outFnc = '', ''
+	if (class_data) then
+		for i,v in pairs(class_data.class_data) do
+			local isFnc = false
+			local strLine = ''			
+			strLine = (
+				strLine
+					..'\n'
+					..perl_components.comment_statement
+					..'Declare '
+			)
+			if (v.data_parameters) then
+				strLine = (
+					strLine
+						..' function as '
+						..tostring(i)
+						..' with parameters: '
+						..commacommacommacommacomma(
+							i_table(
+								v.data_parameters
+							)
+						)
+						..' and return data type '
+						..stringify(v.data_type)
+						..'.'
+						..'\n'
+						..perl_components.sub
+						..code_components.bracketify(
+							commacommacommacommacomma(
+								i_table(
+									v.data_parameters
+								)
+							)
+						)
+				)
+			elseif (type(v.data_value) == "table") then
+				strLine = (
+					strLine
+						..' array '
+						..
+						..' array and assign the value '
+				)
+			else
+				strLine = (
+					strLine
+						..' attribute '
+						..tostring(i)
+						..' as data type '
+				)	
+				if (type(v.data_value) == "string") then
+					strLine = (
+						strLine
+							..stringify(v.data_type)
+					)
+				elseif (type(v.data_value) == "char") then
+					strLine = (
+						strLine
+							..charify(v.data_type)
+					)
+				else
+					print('der_value: perl_class/var/v.data_type')
+					strLine = (
+						strLine
+							..'blahblahblahitsanerror'
+					)
+				end
+				strLine = (
+					strLine
+						..
+				)
+			else
+				
+				
+			end
+		end
+	end
+end
+
+perl_demo_class = demo_class
+
+perl_demo_class_data = perl_class(
+	perl_demo_class
+)
+
+test_file(
+	
+)
--- languages/programming_languages/ruby/ruby.lua
+++ languages/programming_languages/ruby/ruby.lua
@@ -1,3 +1,42 @@
 require "essentialz/essentialz/"
 			
-			
+ruby_components = {
+	class_attr_call = '@';
+}
+
+ruby_class_data = function(class_data) 
+	local outAtr, outFnc = (''), ('')
+	if (class_data) then
+		for i,v in pairs(class_data) do
+			local isFnc = (false)
+			local strLine = ('')
+			if (v.data_parameters) then
+				strLine = (
+					strLine
+						..'\n\t'
+						..ruby_components.comment_statement
+						..' Declare function '
+						..tostring(i)
+						..' and return data type '
+						..stringify(v.data_type)
+						..'.'
+						..'\n\t'
+						..v.data_privacy
+						..' '
+						..v.data_type
+						..' '
+						..tostring(i)
+						..code_components.parenthesisify(commacommacommacommacomma())
+				)
+			end
+		end
+	else
+		print('nil_value: ruby_class_generator')
+	end
+end
+
+ruby_demo_class = demo_class
+
+ruby_demo_class_data = ruby_class(
+	ruby_demo_class
+)
